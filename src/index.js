295d00f6a608d34a1576caecdc6b7ec0b9a9d04a
Rodean <rodeanfraser@gmail.com>
2022-07-08 00:19:04 -0400

    added theme provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { MantineProvider} from '@mantine/core';
 import './index.css';
 import App from './App';
 
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <MantineProvider theme={{ fontFamily: 'Open Sans', colorScheme: 'dark' }} withGlobalStyles withNormalizeCSS >
+        <App />
+      </MantineProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
