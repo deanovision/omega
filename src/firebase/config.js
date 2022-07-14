0b85a1a59d1ccc4ffa92a7dd760d53e21da7abb5
Rodean <rodeanfraser@gmail.com>
2022-07-14 01:12:54 -0400

    added dropzone component

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, setDoc } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider} from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -16,6 +17,7 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider()
+export const storage = getStorage(app)
 
 export const addUser = (user)=> {
   setDoc(collection(db, "users", user.uid), {
