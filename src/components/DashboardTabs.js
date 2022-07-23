19e4784c73d96fcfb55937bb7e42f0e1ac2d8781
Rodean <rodeanfraser@gmail.com>
2022-07-23 14:44:19 -0400

    updated mobile styling for comments list

--- src/components/DashboardTabs.js
+++ src/components/DashboardTabs.js
@@ -1,27 +1,51 @@
 import { useState } from 'react';
-import { Tabs, Card } from '@mantine/core';
+import { Tabs, Card, MediaQuery } from '@mantine/core';
 import { TrendingUp, Notebook, Coin } from 'tabler-icons-react';
 
 function DashboardTabs({timeline, trendingNewsInfo}) {
   const [activeTab, setActiveTab] = useState(0);
   return (
-    <Card withBorder pl={0} pr={0} pt={0} pb={5} p="xl" radius="sm">
-        <Tabs active={activeTab} onTabChange={setActiveTab}>
-            <Tabs.Tab 
-              label="NEWS FEED" 
-              icon={<Notebook size={14} />}>
-                {timeline}
-            </Tabs.Tab>
-            <Tabs.Tab 
-              label="TRENDING" 
-              icon={<TrendingUp size={14} />}>
-                {trendingNewsInfo}
-              </Tabs.Tab>
-            <Tabs.Tab 
-              label="OMEGA" 
-              icon={<Coin size={14} />} />
-        </Tabs>
-    </Card>
+    <>
+      <MediaQuery largerThan="sm" styles={{display: 'none'}}>
+        <Card withBorder pl={0} pr={0} pt={0} pb={5} p="xl" radius="sm">
+            <Tabs active={activeTab} onTabChange={setActiveTab}>
+                <Tabs.Tab 
+                  label="NEWS FEED" 
+                  icon={<Notebook size={14} />}>
+                    {timeline}
+                </Tabs.Tab>
+                <Tabs.Tab 
+                  label="TRENDING" 
+                  icon={<TrendingUp size={14} />}>
+                    {trendingNewsInfo}
+                  </Tabs.Tab>
+                <Tabs.Tab 
+                  label="OMEGA" 
+                  icon={<Coin size={14} />} />
+            </Tabs>
+        </Card>
+      </MediaQuery>
+      {/* Desktop     */}
+      <MediaQuery smallerThan="sm" styles={{display: 'none'}}>
+        <Card withBorder pl={0} pr={0} pt={0} pb={5} p="xl" radius="sm">
+            <Tabs active={activeTab} onTabChange={setActiveTab}>
+                <Tabs.Tab 
+                  label="NEWS FEED" 
+                  icon={<Notebook size={14} />}>
+                    {timeline}
+                </Tabs.Tab>
+                <Tabs.Tab 
+                  label="TRENDING" 
+                  icon={<TrendingUp size={14} />}>
+                    {trendingNewsInfo}
+                  </Tabs.Tab>
+                <Tabs.Tab 
+                  label="OMEGA" 
+                  icon={<Coin size={14} />} />
+            </Tabs>
+        </Card>
+      </MediaQuery>
+    </>
   );
 }
 export default DashboardTabs
