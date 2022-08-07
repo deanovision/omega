622c8a161293b2c47c7515cce9ce941933149218
Rodean <rodeanfraser@gmail.com>
2022-08-07 18:03:24 -0400

    continued work on addMessage function in message model also created helper function to sort uids

--- src/firebase/messageModel.js
+++ src/firebase/messageModel.js
@@ -6,42 +6,36 @@ import {
   collection,
   arrayUnion,
   Timestamp,
-  //   getDoc,
-  //   getDocs,
-  //   query,
-  //   where,
+  getDoc,
+  getDocs,
+  query,
+  where,
   //   orderBy,
   //   updateDoc,
   //   arrayRemove,
   //   writeBatch,
 } from "firebase/firestore";
+import { sortUids } from "../utils/helperFunctions";
 
-// export const addMessage = (authUserData, recipientUserData, message, callback) => {
-//     addDoc(collection(db, "messages"), message)
-//       .then((res) => {
-//         console.log("success, post has been added", res.id);
-//         callback();
-//         updateRecentPosts(uid, post, res.id);
-//         //   window.location.reload()
-//       })
-//       .catch((err) => {
-//         console.error("Error adding document", err);
-//       });
-//   };
-//   export const updateRecentPosts = (uid, post, postId) => {
-//     setDoc(
-//       doc(db, "followers", uid),
-//       {
-//         recentPosts: arrayUnion({ ...post, postId: postId }),
-//         lastPost: Timestamp.now(),
-//         uid: uid,
-//       },
-//       { merge: true }
-//     )
-//       .then(() => {
-//         console.log("success, post has been added to recent posts");
-//       })
-//       .catch((err) => {
-//         console.error("Error adding document", err);
-//       });
-//   };
+export const addMessage = async (
+  authUser,
+  recipient,
+  message = { body: "test" }
+) => {
+  const directMessageDocId = sortUids([authUser.uid, recipient.uid]);
+  const directMessageDocRef = doc(db, "directMessages", directMessageDocId);
+  setDoc(
+    directMessageDocRef,
+    {
+      users: [authUser.uid, recipient.uid],
+      subcollectionRef: await addDoc(
+        collection(db, "directMessages", directMessageDocId, "messages"),
+        {
+          ...message,
+          createdAt: Timestamp.now(),
+        }
+      ),
+    },
+    { merge: true }
+  );
+};
--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -1,15 +1,21 @@
-import { Timestamp } from "firebase/firestore"
-import { formatDistance } from "date-fns"
+import { Timestamp } from "firebase/firestore";
+import { formatDistance } from "date-fns";
 
 export const formatTimestamp = (seconds, nanoseconds) => {
-    const postCreatedAt = new Timestamp(seconds, nanoseconds).toDate()
-    return relativeTime(postCreatedAt)
-}
+  const postCreatedAt = new Timestamp(seconds, nanoseconds).toDate();
+  return relativeTime(postCreatedAt);
+};
 export const relativeTime = (time) => {
-    let result = formatDistance(
-        new Date(time),
-        new Date(),
-        { addSuffix: true }
-      )
-      return result
-}
+  let result = formatDistance(new Date(time), new Date(), { addSuffix: true });
+  return result;
+};
+export const sortUids = (uidArray) => {
+  const collator = new Intl.Collator(undefined, {
+    numeric: true,
+    sensitivity: "base",
+  });
+  const sortedUidArray = uidArray.sort((a, b) => {
+    return collator.compare(a, b);
+  });
+  return `${sortedUidArray[0]}${sortedUidArray[1]}`;
+};
